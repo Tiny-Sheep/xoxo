b3c5592bfbb4291a3bd3bd76ebc2359cfa32c65b
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-05-16 11:45:29 -0500

    lamine

--- game/index.js
+++ game/index.js
@@ -1,6 +1,23 @@
 import {Map} from 'immutable'
+import {createStore} from "redux"
 
-export default function reducer(state, action) {
-  // TODO
+const defaultBoard = Map()
+
+const playerX= ()=>{store.dispatch({type:"MOVE", postion:[0,0], player:"X"});
+const playerO= ()=>{store.dispatch({type:"MOVE", postion:[0,0], player:"O"});
+
+
+
+
+export default function reducer(state={board:defaultBoard}, action) {
+
+
+ switch (action.type){
+   case "X":
+
+ } 
+
+
+
   return state
 }
